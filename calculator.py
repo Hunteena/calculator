5ed41616059ac0734739dc429e608b4e5e779fdb
Nina Speranskaya <nina-sp@list.ru>
2024-02-29 01:00:44 +0600

    [+] operations log

--- calculator.py
+++ calculator.py
@@ -1,8 +1,17 @@
+import logging
+from collections import deque
+
 from operations import add, sub, mul, div, mod, power, sqrt, solve, InvalidInputError
 
+LOG_FILE = 'calculator.log'
 
-# TODO Необходимо сохранять историю (ввод/вывод)
 # TODO Копирование в буфер обмена по клавишам ctrl+c результата вычислений
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(message)s',
+    encoding='utf-8',
+    filename=LOG_FILE,
+)
 
 
 def main(*task: str) -> str:
@@ -43,12 +52,24 @@ def main(*task: str) -> str:
         raise InvalidInputError(f"{oper} должно быть оператором")
 
 
+def get_log(s: str) -> None:
+    try:
+        n = int(s)
+    except ValueError:
+        raise InvalidInputError("Укажите количество операций для вывода")
+    if n == 0:
+        raise InvalidInputError("Укажите количество операций для вывода")
+    with open(LOG_FILE) as log:
+        lines = deque(log, 2 * n)
+    for line in lines:
+        print(line, end='')
+
+
 if __name__ == "__main__":
     print("Введите пример или простое уравнение, разделяя числа и операторы пробелами.\n"
-          "Чтобы получить справку, введите 'help' или просто нажмите ввод.")
+          "Чтобы получить справку, просто нажмите ввод.")
     while True:
-        print("Введите пример:")
-        input_data = input().strip()
+        input_data = input("Введите пример: ").strip()
         if input_data == "exit":
             print("Калькулятор завершил работу.")
             break
@@ -64,9 +85,18 @@ if __name__ == "__main__":
                   "    ^ 1/2 извлечение квадратного корня,\n"
                   "    решение простых уравнений с одним неизвестным, например, 2 + x = 14.\n"
                   "Для завершения работы введите 'exit'.\n"
-                  "Чтобы получить эту справку, введите 'help' или просто нажмите ввод.")
-        else:
+                  "Чтобы увидеть последние n операций, введите 'h <n>'.\n"
+                  "Чтобы получить эту справку, просто нажмите ввод или введите пустую строку.")
+        elif input_data.startswith('h'):
             try:
-                print(main(*input_data.split()))
+                get_log(input_data[1:])
             except InvalidInputError as e:
                 print(e.message)
+        else:
+            logging.info(f">>> {input_data}")
+            try:
+                result = main(*input_data.split())
+            except InvalidInputError as e:
+                result = e.message
+            print(result)
+            logging.info(result)
